2dd199395f4d844260b55b7fda94e165f50ccba6
Reuben <reubenamuzu23@gmail.com>
2025-11-11 19:31:14 +0000

    Protected routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { loginUser, signUp } from "../controller/userController.js";
+import { authUser } from "../Middleware/authUser.js";
 
 const userRoute = express.Router();
 
 userRoute.post("/signup", signUp);
 userRoute.post("/login", loginUser);
+userRoute.get("/dashboard", authUser, (req, res) => {
+  res.send(`Welcome ${req.user.id}`);
+});
 
 export default userRoute;
